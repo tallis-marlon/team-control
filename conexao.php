defac8cc575c79617faeff9ab274e06ebf12899a
Tallis Marlon <marlontallis@gmail.com>
2023-04-11 22:07:26 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -5,7 +5,7 @@ $usuario = 'root';
 $senha = '';
 $database = 'team_control';
 
-$mysqli = new mysqli($host, $usuario, $senha, $database);
+$conn = new mysqli($host, $usuario, $senha, $database);
 
 if ($conn->error) {
     die("Falha ao conectar" . $conn->error); /*A função die() em PHP é usada para imprimir uma mensagem e encerrar a execução do script atual. É essencialmente a mesma coisa que a função exit(), que é usada para interromper a execução de um script */
